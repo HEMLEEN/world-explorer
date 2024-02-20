442eb1994445c51d0ee8d724907dfdceeb43cf52
Hemleen <hemleen7@gmail.com>
2024-02-21 02:50:28 +0530

    ref ; styles update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,11 +9,11 @@ const Home = () => {
     <>
   <Header headerText = 'Road-Map-FullStack'/>
     <div className='mainContainer'>
+   
       <div className='AppContent'>
-           <Link className='Link' to='/frontend'><h2>Frontend Development</h2></Link>
-      </div>
-      <div className='AppContent'>
+      <Link className='Link' to='/frontend'><h2>Frontend Development</h2></Link>
       <Link className='Link' to='/backend'><h2>Backend Development</h2></Link>
+
       </div>
       
     </div>
