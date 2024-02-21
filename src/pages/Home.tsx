ad0e5c8bd79be1d9a6202706f393034d25cf15ff
Hemleen <hemleen7@gmail.com>
2024-02-22 01:23:18 +0530

    home style changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,14 +8,10 @@ const Home = () => {
   return (
     <>
   <Header headerText = 'Road-Map-FullStack'/>
-    <div className='mainContainer'>
    
       <div className='AppContent'>
       <Link className='Link' to='/frontend'><h2>Frontend Development</h2></Link>
-      <Link className='Link' to='/backend'><h2>Backend Development</h2></Link>
-
-      </div>
-      
+      <Link className='Link' to='/backend'><h2>Backend Development</h2></Link>      
     </div>
 
     </>
